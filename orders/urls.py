4d93b8220afd9737aeec8ba66dffc0504d879c50
ducpham01 <ducpa.01@icloud.com>
2023-03-15 10:39:02 +0700

    place order

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "orders"
 urlpatterns = [
-    path('<int:id_pitch>/', views.index, name='index'),
+    path('<int:id_pitch>/', views.index, name='orders'),
+    path('<int:id_pitch>/place_order',views.place_order,name="place_order")
 ]
--- orders/views.py
+++ orders/views.py
@@ -2,10 +2,37 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render,get_list_or_404
+from accounts.models import Account
+from equipments.models import Equipment
+
+from footballpitchs.models import FootballPitch
+from orders.models import Order
 
 
 def index(request, id_pitch):
-    # question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'orders/index.html', {"id_pitch": id_pitch})
-    # return HttpResponse("You're voting on question %s." % id_pitch)
+    football_pitch = get_object_or_404(FootballPitch, pk=id_pitch)
+    equipments = get_list_or_404(Equipment)
+    return render(request, 'orders/index.html', {"id_pitch": id_pitch, "FootballPitch":football_pitch,"equipments":equipments})
+
+def place_order(request, id_pitch):
+    user_name = request.POST["user_name"]
+    time_slot = request.POST["time_slot"]
+    ordered_date = request.POST["ordered_date"]
+    football_pitch = get_object_or_404(FootballPitch, pk=id_pitch)
+        
+    user = Account.objects.get(user_name = user_name )
+    if user is None:
+        user = Account(user_name = user_name)
+        user.save()
+    
+    success = True
+    try:
+        
+        order = Order(id_pitch=football_pitch, time_slot = time_slot, 
+                    ordered_date = ordered_date, user_name=user)
+        
+        order.save()
+    except:
+        success  = False
+    return render(request, "orders/place_order_result.html",{"success":success})
